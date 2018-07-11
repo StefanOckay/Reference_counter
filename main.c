a8cc44ee879467da9a8130e8f04e22e012d17644
Stefan Ockay <stefan@localhost.localdomain>
2018-07-11 23:11:01 +0200

    repair mainly main

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
+/**
 #include "memoryVM.h"
 #include "memoryVMObject.h"
+**/
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 
 typedef struct _safeLogger
@@ -28,22 +33,13 @@ void closeSafeLogger(void* memory)
 
 int main()
 {
-    //void (*f)(void *);
-    size_t *counter = malloc(sizeof(void *) + sizeof(size_t));
-    *counter = 1;
-    printf("counter: %zu\n", *counter);
-    void (*f)(void *) = (void *)(counter + 1);
-    f = *closeSafeLogger;
+    void (**f)(void *) = malloc(sizeof(void (*)(void *)) + sizeof(size_t));
+    size_t *counter = (size_t *)(f + 1);
+    *f = closeSafeLogger;
     safeLogger logger;
     logger.file = fopen("testfile.txt", "a");
-    f(&logger);
-    return 0;
-    int *array = allocate(10 * sizeof(int));
-    for (int i = 0; i < 10; i++) {
-        array[i] = i + 1;
-        printf("%i\n", array[i]);
-    }
-    printf("%zu\n", *((size_t *)array - 1));
-    printf("%i\n", array[3]);
+    (*f)(&logger);
+    *counter = 1;
+    printf("counter: %zu\n", *counter);
     return 0;
 }
